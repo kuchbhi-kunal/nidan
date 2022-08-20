b4f7c1f05854bd00f7ab3b6b1246be4ecb054a38
Kunal Thakkar <78646652+kuchbhi-kunal@users.noreply.github.com>
2022-08-20 23:49:01 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-var mySong = document.getElementById("mySong")
-var button = document.getElementById ("song")
-
-button.onclick = function(){
-    if (mySong.paused){
-        mySong.play();
-    }else
-    mySong.pause();
-}
